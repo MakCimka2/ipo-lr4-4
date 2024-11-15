660d53c3e81bddfeb94be2451e1b69c9292c148c
MakCimka <poduskapuska@gmail.com>
2024-11-15 15:23:01 +0300

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 n = int(input("Введите число: "))
-list1 = [x for x in range(1, n + 1)if x % 5 == 0]
+list1 = [x for x in range(1, n + 1)if x % 5 == 0] #Создание списка чисел которрые делятся на 5
 print(list1) 
